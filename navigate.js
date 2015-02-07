acb3b361dfd56602db0ae1312a9fefd2b1b56d2a
Stanisław Kacprzak <stan.kacprzak@gmail.com>
2015-02-07 18:33:43 +0100

    code formating, changes in manifest

--- navigate.js
+++ navigate.js
@@ -1,22 +1,25 @@
 chrome.commands.onCommand.addListener(function(command) {
-chrome.tabs.getSelected(null,function(tab) {
-url = tab.url;
-parts = url.split("/");
-lastPart = parts[parts.length-1];
+    chrome.tabs.getSelected(null, function(tab) {
+        url = tab.url;
+        parts = url.split("/");
+        lastPart = parts[parts.length - 1];
 
-parts=lastPart.split("?");
-if(parts.length == 2)
-	lastPart = parts[1]
+        parts = lastPart.split("?");
+        if (parts.length == 2)
+            lastPart = parts[1]
 
-stringNumberInName = lastPart.replace(/[^0-9]/gi, '');
-numberInName = Number(stringNumberInName);
-if(command == "navigate_plus")
-	newNumberInName = numberInName + 1;
-if(command == "navigate_minus" && numberInName > 1)
-	newNumberInName = numberInName - 1;
+        stringNumberInName = lastPart.replace(/[^0-9]/gi, '');
+        numberInName = Number(stringNumberInName);
 
-url = url.replace(lastPart, lastPart.replace(stringNumberInName,newNumberInName));
-chrome.tabs.update(tab.id, {url: url});
-});
+        if (command == "navigate_plus" && numberInName > 0)
+            newNumberInName = numberInName + 1;
+        if (command == "navigate_minus" && numberInName > 1)
+            newNumberInName = numberInName - 1;
+
+        url = url.replace(lastPart, lastPart.replace(stringNumberInName, newNumberInName));
+        chrome.tabs.update(tab.id, {
+            url: url
+        });
+    });
 });
 
